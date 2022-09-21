863da2d3655ba45fc430594d69f3146fa1be09e1
dumer27 <dumasdumasflow@gmail.com>
2022-09-21 14:49:28 -0500

    Crear partido

--- Torneo.App.Frontend/Pages/Partidos/Create.cshtml.cs
+++ Torneo.App.Frontend/Pages/Partidos/Create.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
 
 namespace Torneo.App.Frontend.Pages.Partidos
 {
     public class CreateModel : PageModel
     {
+        private readonly IRepositorioPartido _repoPartido;
+        public Partido partido {get; set;}
+        public CreateModel(IRepositorioPartido repoPartido)
+        {
+            _repoPartido = repoPartido;
+            
+        }
+
+
         public void OnGet()
         {
+            partido = new Partido();
+           
+
         }
+        
     }
 }
